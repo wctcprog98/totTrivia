8a43b3a84584a12d03c008ee794610bcaa920582
Nenye <chinenyediei@gmail.com>
2020-06-27 09:45:51 -0500

    initial commit

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+//trivia arrays
+
+let sports = [
+    {
+        "question": "In professional basketball, how high is the basketball hoop from the ground?",
+        "answer": "10 feet"
+    },
+    {
+        "question": "In professional basketball how high is the basketball hoop from the ground?",
+        "answer": "10 feet"
+    },
+]
+    
+$(document).ready(function () {
+    
+});
